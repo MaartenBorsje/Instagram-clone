7046de4d847858f4c3be67bb51aeed44a1251dfd
MaartenBorsje <59605923+MaartenBorsje@users.noreply.github.com>
2022-06-19 17:49:27 +0200

    Basic setup of the profile page - structure (part 8)

---
+++ Instagram/Models/Models.swift
@@ -0,0 +1,65 @@
+//
+//  Models.swift
+//  Instagram
+//
+//  Created by Maarten Borsje on 19/06/2022.
+//
+
+import Foundation
+
+enum Gender {
+    case male, female, other
+}
+
+struct User {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthDate: Date
+    let gender: Gender
+    let counts: UserCount
+    let joinDate: Date
+}
+
+struct UserCount {
+    let followers: Int
+    let following: Int
+    let posts: Int
+}
+
+public enum UserPostType {
+    case phot, video
+}
+
+/// Represents a user post
+public struct UserPost {
+    let idenifier: String
+    let postType: UserPostType
+    let thumnailImage: URL
+    let postUrl: URL
+    let caption: String?
+    let likeCount: [PostLike]
+    let comments: [PostComment]
+    let createdDate: Date
+    let taggedUsers: [String]
+    // add here camera info tab
+    // Optionally you can add a specific section for hastags
+}
+
+struct PostLike {
+    let username: String
+    let postIdentifier: String
+}
+
+struct CommentLike {
+    let username: String
+    let commentIdentifier: String
+}
+
+struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
+}

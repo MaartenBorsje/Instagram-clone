f0101bb59209d7ef47795b51106dd215fd57b0f0
MaartenBorsje <59605923+MaartenBorsje@users.noreply.github.com>
2022-06-27 19:53:46 +0200

    Setting up the explore page (part 16/1)

--- Instagram/Controllers/Core Tabs/ExploreViewController.swift
+++ Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -11,9 +11,12 @@ class ExploreViewController: UIViewController {
 
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
+        searchBar.placeholder = "Search" // maybe change it to discover
         searchBar.backgroundColor = .secondarySystemBackground
         return searchBar
     }()
+    
+    private var models = [UserPost]()
     
     private var collectionView: UICollectionView?
     
@@ -25,7 +28,12 @@ class ExploreViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: (view.width-4)/3, height: (view.width-4)/3)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(PhotoCollectionViewCell.self,
+                                 forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         collectionView?.delegate = self
         collectionView?.dataSource = self
         guard let collectionView = collectionView else {
@@ -33,18 +41,64 @@ class ExploreViewController: UIViewController {
         }
 
         view.addSubview(collectionView)
+        
+        searchBar.delegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
+    }
+}
 
+extension ExploreViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        didCancelSearch()
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        
+        query(text)
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(didCancelSearch))
+    }
+    
+    @objc private func didCancelSearch() {
+        searchBar.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+    }
+    
+    private func query(_ text: String) {
+        // perform the search in the back-end
+    }
 }
 
 extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 100
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier,
+                                                            for: indexPath) as? PhotoCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        //cell.configure(with: )
+        cell.configure(debug: "test")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        //let model = models[indexPath.row]
+        let vc = PostViewController(model: nil)
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 }
